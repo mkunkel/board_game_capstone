e69d011990d88b5462f826c9696e2b1c3e4841cd
Mike Kunkel <mike@mkunkel.com>
2014-01-30 17:47:58 -0600

    Play model unit tests

---
+++ spec/models/play_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+require_relative '../../lib/environment'
+require_relative '../../models/play'
+
+describe Play do
+  before(:each) do
+    Environment.test_prepare
+    @play = Play.new
+  end
+
+  context 'class methods' do
+    describe '.new' do
+      it "Should initialize an instance of class Play" do
+        @Play.class.should be Play
+      end
+    end
+  end
+
+  context 'instance methods' do
+    describe '#save' do
+      it "Should save plays to the database" do
+        @play.save
+        Play.count.should be 1
+      end
+    end
+
+    describe 'valid?' do
+      it "should return true if Play has required attributes" do
+        @friend.should be_valid
+      end
+
+      it "should return false if friend doesn't have required attributes" do
+        p = Play.new
+        p.should_not be_valid
+      end
+
+      it "should populate errors on the instance if invalid" do
+        p = Play.new
+        p.valid?
+        p.errors.count.should_not be 0
+      end
+    end
+  end
+end
